c52c2d560c010748d7a0fce75a40d29b5b2238fa
Pragati Chaudhary <savita845822@gmail.com>
2026-01-28 22:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
+#app.py is entry point
 import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from joblib import load
+from joblib import load #pretrained ml model saved in file is loaded
 
 # Load the trained model and preprocessing pipeline
-import os
+import os #to interacct wih the os
 import tensorflow as tf
 
 # Get current directory dynamically
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, 'pythonproject-main', 'cybersecurity_model.h5')
-
 model = tf.keras.models.load_model(MODEL_PATH)
 
 
@@ -45,6 +45,7 @@ vehicle_model = st.selectbox('Vehicle Model', ['Model A', 'Model B', 'Model C'])
 firmware_version = st.selectbox('Firmware Version', ['v1.0', 'v2.0', 'v3.0'])
 geofencing_status = st.selectbox('Geofencing Status', ['Enabled', 'Disabled'])
 
+#now user press the button predict threat
 if st.button("Predict Threat"):
     try:
         # Create a DataFrame for the input
@@ -54,18 +55,18 @@ if st.button("Predict Threat"):
         )
         
         # Preprocess the input data
-        input_data_processed = preprocessor.transform(input_data)
+        input_data_processed = preprocessor.transform(input_data) #converting the raw data in the numerical format
         
         # Ensure input_data_processed has shape (1, 16) for the model
         input_data_processed = np.reshape(input_data_processed, (1, -1))  # Reshape to (1, 16)
         
         # Assuming binary classification with a positive label
-        y_true = np.array([1])
+        y_true = np.array([1]) #assuming that this is attack
         
         # Generate adversarial example
         input_data_processed_adv = generate_adversarial_examples(model, input_data_processed, y_true)
         
-        # Ensure the data is in batch format (reshape to (1, 16))
+        # Ensure the data is in batch format (reshape to (1, 16)) #this means converting to table format 
         input_data_processed_adv = np.reshape(input_data_processed_adv, (1, -1))
         
         # Make a prediction
@@ -79,3 +80,6 @@ if st.button("Predict Threat"):
     
     except Exception as e:
         st.error(f"An unexpected error occurred: {e}")
+#The model is already trained and validated on clean data. In app.py, Streamlit is used only for interaction, while adversarial examples
+#are generated to test how robust the trained model is against manipulated or attacked inputs
+#why we are predicting on the real world data this might be possible ki actual data thoda sa biased aaye due to attacker or hacker so we are calculationg for the nearby 
